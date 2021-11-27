a93287cc258b9ec91e24797ac5744c0e0034b454
shadow-chaser <ashabhtanim@gmail.com>
2021-11-27 14:29:19 +0600

    updated

--- index.js
+++ index.js
@@ -17,25 +17,73 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const usersCollection = client.db(`${process.env.DB_NAME}`).collection("users");
+  const productsCollection = client.db(`${process.env.DB_NAME}`).collection("products");
+  const verifiedUsersCollection = client.db(`${process.env.DB_NAME}`).collection("verifiedUsers");
+
+  
   // perform actions on the collection object
-  
-   // add user to database
-   app.post('/addUser', (req, res) => {
+
+  // add user to database
+  app.post('/addUser', (req, res) => {
     usersCollection.insertOne(req.body)
-        .then(result => {
-            res.send(true)
-                })
-    });
+      .then(result => {
+        res.send(true)
+      })
+  });
+
+  // add products to database
+  app.post('/addProduct', (req, res) => {
+    productsCollection.insertOne(req.body)
+      .then(result => {
+        res.send(true)
+      })
+  });
+
+  //getting products 
+  app.get('/products', (req, res) => {
+    productsCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
+
+  // verify user
+
+  app.get('/verifyUser', (req, res) => {
+    const queryEmail = req.query.email;
+    usersCollection.find({ email: queryEmail })
+        .toArray((err, users) => {
+            verifiedUsersCollection.insertOne(users[0])
+            .then(result => {
+                    res.send(true)
+                  })
+        })
+})
+  //  app.get('/verifyUser', (req, res) => {
+  //   const queryEmail = req.query.email;
+  //   usersCollection.find({ email: queryEmail })
+  //   .toArray((err, user) => {
+
+  //   }
+
+  //   console.log(user.body);
+  //   verifiedUsersCollection.insertOne(user)
+  //     .then(result => {
+  //       res.send(true)
+  //     })
+  // });
+
+ 
 
 
-  
+
 });
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Trade Bidders!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+  res.send('Hello Trade Bidders!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
