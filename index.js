c54afc860613524df50f14363380baf4079acbfc
shadow-chaser <ashabhtanim@gmail.com>
2021-11-27 12:32:05 +0600

    updated

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ app.use(cors());
 app.use(bodyParser.json());
 // app.use(fileUpload());
 
-
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8cui.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
+// ${process.env.DB_USER}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8cui.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const usersCollection = client.db(`${process.env.DB_NAME}`).collection("user");
+  const usersCollection = client.db(`${process.env.DB_NAME}`).collection("users");
   // perform actions on the collection object
   
    // add user to database
    app.post('/addUser', (req, res) => {
     usersCollection.insertOne(req.body)
         .then(result => {
-            res.send(result.insertedCount > 0)
+            res.send(true)
         })
-});
+    });
 
 
   
@@ -33,7 +33,7 @@ client.connect(err => {
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Trade Bidders!')
   })
   
   app.listen(port, () => {
